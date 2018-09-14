864a6148c1db1f2eaca783f6600053134f540368
adarshj <adarshj@qburst.com>
2018-09-14 14:59:04 +0530

    heroku final commit & webworker url change....................

--- src/webworker/webworker.js
+++ src/webworker/webworker.js
@@ -2,7 +2,7 @@
 const workercode = () => {
 
     let onmessage = function (e) {
-
+        console.log("Update request recieved")
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
