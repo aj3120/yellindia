463b23b69ecee30423c2775593e0795441730dfe
adarshj <adarshj@qburst.com>
2018-09-14 15:20:53 +0530

    sonar exclude

--- src/webworker/webworker.js
+++ src/webworker/webworker.js
@@ -2,7 +2,6 @@
 const workercode = () => {
 
     let onmessage = function (e) {
-        console.log("Update request recieved")
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
